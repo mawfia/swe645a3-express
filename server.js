db088289c4e2033ead8dfcec6f2a300346fcc700
Andrew <michael.a.williams@mawfia.com>
2020-11-03 00:50:58 -0500

    First Jenkins run with express

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const request = require('request');
+const got = require('got');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+const api = process.env.PROD ? "http://10.100.113.23:8080/swe645a3/api" : "http://localhost:8080/swe645a3/api";
+
+app.get('/proxy/surveys', (req, res) => {
+  (async () => {
+      try {
+          const response = await got.get(`${api}/surveys`);
+          //console.log(response.body);
+          res.json({'surveys' : JSON.parse(response.body)});
+          //=> '<!doctype html> ...'
+      } catch (error) {
+          console.log(error.response.body);
+          return res.status(500).json({ type: 'error', message: error.response.body });
+          //=> 'Internal server error ...'
+      }
+  })();
+
+});
+
+app.get('/proxy/test_proxy', (req, res) => {
+  return res.status(200).json({happy:"Good test"});
+})
+
+app.get('/proxy/test_api', (req, res) => {
+  (async () => {
+      try {
+          const response = await got.get(`${api}/test`);
+          //console.log(response.body);
+          res.json({'test' : JSON.parse(response.body)});
+          //=> '<!doctype html> ...'
+      } catch (error) {
+          console.log(error.response.body);
+          return res.status(500).json({ type: 'error', message: error.response.body });
+          //=> 'Internal server error ...'
+      }
+  })();
+})
+
+app.post('/proxy/survey', (req, res) => {
+
+  (async () => {
+      try {
+          const response = await got.get(`${api}/survey/${req.body.id}`);
+          //console.log(response.body);
+          return res.status(200).json({success:JSON.parse(response.body)});
+          //=> '<!doctype html> ...'
+      } catch (error) {
+          console.log(error.response.body);
+          return res.status(500).json({ error: error.response.body });
+          //=> 'Internal server error ...'
+      }
+  })();
+
+  //res.status(200);
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
